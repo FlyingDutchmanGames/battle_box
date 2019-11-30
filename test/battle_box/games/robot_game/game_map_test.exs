c29053edbebb9f34e8974f9a35a4123dd6037039
Grant Powell <gjamespowell@gmail.com>
2019-11-29 23:57:34 -0500

    Create a constant for the default game map

---
+++ test/battle_box/games/robot_game/game_map_test.exs
@@ -0,0 +1,20 @@
+defmodule BattleBox.Games.RobotGame.GameMapTest do
+  use ExUnit.Case
+  alias BattleBox.Games.RobotGame.GameMap
+
+  describe "default" do
+    test "it has the correct number of spaces" do
+      map = GameMap.default
+      assert length(Enum.into(map.terrain, [])) == 361
+    end
+
+    test "all the spaces are represented" do
+      map = GameMap.default
+
+      expected_positions = for row <- 0..18, col <- 0..18, do: {row, col}
+      actual_positions = Map.keys(map.terrain)
+
+      assert Enum.sort(expected_positions) == Enum.sort(actual_positions)
+    end
+  end
+end
