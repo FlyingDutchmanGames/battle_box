d086f0a3ebab112ec7b7e5c4fb5180a3e37a90c3
Grant Powell <gjamespowell@gmail.com>
2019-11-30 15:49:57 -0500

    Basic Logic Module, and `spawning_round?` function

---
+++ lib/battle_box/games/robot_game/logic.ex
@@ -0,0 +1,12 @@
+defmodule BattleBox.Games.RobotGame.Logic do
+  def calculate_turn(game, moves) do
+    game = if spawning_round?(game), do: apply_spawn(game), else: game
+  end
+
+  def apply_spawn(_game) do
+    raise "Not Implemented"
+  end
+
+  def spawning_round?(%{settings: %{spawn_every: spawn_every}, turn: turn}),
+    do: rem(turn, spawn_every) == 0
+end
