7dbd47ac1a503c425e5426cce8085888f994c7ac
Grant Powell <gjamespowell@gmail.com>
2019-12-06 00:14:31 -0500

    Refactor the Logic, its awesome

--- lib/battle_box/games/robot_game/logic.ex
+++ lib/battle_box/games/robot_game/logic.ex
@@ -7,94 +7,97 @@ defmodule BattleBox.Games.RobotGame.Logic do
         do: apply_spawn(game),
         else: game
 
-    grouped_moves = Enum.group_by(moves, fn move -> move.type end)
+    guarded_locations =
+      for move <- moves,
+          robot = get_robot(game, move.robot_id),
+          move.type == :guard,
+          do: robot.location
 
-    guard_locations =
-      for %{robot_id: id} <- grouped_moves[:guard] || [],
-          %{location: location} = get_robot(game, id),
-          do: location
-
-    movements = grouped_moves[:move] || []
-
-    move_results =
-      Enum.map(movements, fn movement -> apply_movement(game, movement, movements) end)
+    movements =
+      for move <- moves,
+          move.type == :move,
+          do: move
 
     game =
-      Enum.reduce(move_results, game, fn move_result, game ->
-        case move_result do
-          {:no_move, {:collision, reason}, robot}
-          when reason in [:invalid_terrain, :multiple_attempting_to_enter] ->
-            apply_damage_to_robot(game, robot.id, collision_damage(game))
-
-          {:no_move, {:collision, {:robot_collision, collided_robot}}, robot} ->
-            game
-            |> apply_damage_to_robot(robot.id, collision_damage(game))
-            |> apply_damage_to_robot(
-              collided_robot.id,
-              if(collided_robot.location in guard_locations,
-                do: guarded_collision_damage(game),
-                else: collision_damage(game)
-              )
-            )
-
-          {:move, target, robot} ->
-            move_robot(game, robot.id, target)
+      Enum.reduce(moves, game, fn move, game ->
+        case move.type do
+          :move -> apply_movement(game, move, movements, guarded_locations)
+          :attack -> apply_attack(game, move.target, guarded_locations)
+          :suicide -> apply_suicide(game, move.robot_id, guarded_locations)
+          _ -> game
         end
-      end)
-
-    game =
-      Enum.reduce(grouped_moves[:attack] || [], game, fn attack, game ->
-        apply_attack(game, attack.target, guard_locations)
-      end)
-
-    game =
-      Enum.reduce(grouped_moves[:suicide] || [], game, fn suicide, game ->
-        apply_suicide(game, suicide.robot_id, guard_locations)
       end)
 
     update_in(game.turn, &(&1 + 1))
   end
 
-  def apply_movement(game, move, movements, stuck_robots \\ []) do
+  def apply_movement(game, move, movements, guarded_locations) do
+    case calc_movement(game, move, movements) do
+      {:no_move, reason, robot} ->
+        case reason do
+          :invalid_terrain ->
+            apply_damage_to_robot(game, robot.id, collision_damage(game))
+
+          :contention ->
+            apply_damage_to_robot(game, robot.id, collision_damage(game))
+
+          {:collision, other_robot} ->
+            other_robot_damage =
+              if other_robot.location in guarded_locations,
+                do: guarded_collision_damage(game),
+                else: collision_damage(game)
+
+            game
+            |> apply_damage_to_robot(robot.id, collision_damage(game))
+            |> apply_damage_to_robot(other_robot.id, other_robot_damage)
+        end
+
+      {:move, target, robot} ->
+        move_robot(game, robot.id, target)
+    end
+
+  end
+
+  def calc_movement(game, move, movements, stuck_robots \\ []) do
     robot = get_robot(game, move.robot_id)
+    robot_currently_at_location = get_robot_at_location(game, move.target) 
+    moves_to_location =Enum.filter(movements, & &1.target == move.target)
 
-    with {:valid_terrain?, true} <- {:valid_terrain?, valid_terrain?(game, move.target)},
-         {:contention?, false} <-
-           {:contention?, attempts_to_enter_space(movements, move.target) > 1},
-         {:current_occupant, nil} <- {:current_occupant, get_robot_at_location(game, move.target)} do
-      {:move, move.target, robot}
-    else
-      {:valid_terrain?, false} ->
-        {:no_move, {:collision, :invalid_terrain}, robot}
+    space_info = %{
+      valid_terrain?: game.terrain[move.target] in [:normal, :spawn],
+      contention?: length(moves_to_location) > 1,
+      current_occupant: robot_currently_at_location,
+      current_occupant_in_stuck_robots?: (if robot_currently_at_location, do:
+        robot_currently_at_location.id in stuck_robots),
+      current_occupant_move: (if robot_currently_at_location,
+        do: Enum.find(movements, & &1.robot_id == robot_currently_at_location.id))
+    }
 
-      {:contention?, true} ->
-        {:no_move, {:collision, :multiple_attempting_to_enter}, robot}
+    case space_info do
+      %{valid_terrain?: false} ->
+        {:no_move, :invalid_terrain, robot}
 
-      {:current_occupant, occupying_robot} ->
-        if occupying_robot in stuck_robots do
-          {:move, move.target, robot}
-        else
-          with occupying_robot_move when not is_nil(occupying_robot_move) <-
-                 Enum.find(movements, fn movement ->
-                   movement.robot_id == occupying_robot.id
-                 end),
-               {:move, _, _} <-
-                 apply_movement(game, occupying_robot_move, movements, [robot | stuck_robots]) do
+      %{contention?: true} ->
+        {:no_move, :contention, robot} 
+
+      %{current_occupant: other_robot, current_occupant_move: nil} when not is_nil(other_robot) ->
+        {:no_move, {:collision, other_robot}, robot}
+
+      %{current_occupant_in_stuck_robots?: true} ->
+        {:move, move.target, robot}
+
+      %{valid_terrain?: true, contention?: false, current_occupant: nil} ->
+        {:move, move.target, robot}
+
+      %{current_occupant: other_robot, current_occupant_move: other_robot_move} ->
+        case calc_movement(game, other_robot_move, movements, [robot.id | stuck_robots]) do
+          {:move, _, _} ->
             {:move, move.target, robot}
-          else
-            _ ->
-              {:no_move, {:collision, {:robot_collision, occupying_robot}}, robot}
-          end
+
+          {:no_move, _, _} ->
+            {:no_move, {:collision, other_robot}, robot}
         end
     end
-  end
-
-  def attempts_to_enter_space(movements, location) do
-    length(Enum.filter(movements, &(&1.target == location)))
-  end
-
-  def valid_terrain?(game, location) do
-    game.terrain[location] in [:normal, :spawn]
   end
 
   def apply_attack(game, location, guard_locations) do
