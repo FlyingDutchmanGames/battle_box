7574f50bb9636b82140f5e9b2658c51ee46fc372
Grant Powell <gjamespowell@gmail.com>
2019-12-08 20:38:10 -0500

    Render robot health and player

--- lib/battle_box_web/live/robot_game/play_live.ex
+++ lib/battle_box_web/live/robot_game/play_live.ex
@@ -5,8 +5,13 @@ defmodule BattleBoxWeb.RobotGame.PlayLive do
 
   def mount(_session, socket) do
     game = Game.new()
-    robot = %{player_id: 10, location: {10, 10}}
-    game = Game.add_robot(game, robot)
+
+    robots = [
+      %{player_id: "player_1", location: {9, 9}},
+      %{player_id: "player_2", location: {10, 10}}
+    ]
+
+    game = Game.add_robots(game, robots)
     {:ok, assign(socket, game: game)}
   end
 
