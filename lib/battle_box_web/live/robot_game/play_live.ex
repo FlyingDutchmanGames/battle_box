a5000036e08e6ac89bb3ece46816d0b4aa6181e6
Grant Powell <gjamespowell@gmail.com>
2019-12-08 21:29:55 -0500

    Build out the concept of a selected robot

--- lib/battle_box_web/live/robot_game/play_live.ex
+++ lib/battle_box_web/live/robot_game/play_live.ex
@@ -4,6 +4,19 @@ defmodule BattleBoxWeb.RobotGame.PlayLive do
   use Phoenix.LiveView
 
   def mount(_session, socket) do
+    {:ok, initialize(socket)}
+  end
+
+  def handle_event("select_robot", %{"robot-id" => robot_id}, socket) do
+    new_selected = if robot_id == socket.assigns.selected, do: nil, else: robot_id
+    {:noreply, assign(socket, selected: new_selected)}
+  end
+
+  def render(assigns) do
+    RobotGameView.render("play.html", assigns)
+  end
+
+  def initialize(socket) do
     game = Game.new()
 
     robots = [
@@ -12,10 +25,6 @@ defmodule BattleBoxWeb.RobotGame.PlayLive do
     ]
 
     game = Game.add_robots(game, robots)
-    {:ok, assign(socket, game: game)}
-  end
-
-  def render(assigns) do
-    RobotGameView.render("play.html", assigns)
+    assign(socket, selected: nil, game: game)
   end
 end
