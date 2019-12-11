31d9225543ef4c9ccf5e4ad3c3a8a64452748bb1
Grant Powell <gjamespowell@gmail.com>
2019-12-10 20:05:46 -0500

    Test Terrain Rendering

--- test/battle_box_web/views/robot_game_view_test.exs
+++ test/battle_box_web/views/robot_game_view_test.exs
@@ -1,6 +1,7 @@
 defmodule BattleBoxWeb.RobotGameViewTest do
   use BattleBoxWeb.ConnCase, async: true
   alias BattleBoxWeb.RobotGameView
+  import Phoenix.View
 
   describe "terrain_number/1" do
     test "the terrain numbering of a starting column is displayed" do
@@ -13,6 +14,31 @@ defmodule BattleBoxWeb.RobotGameViewTest do
 
     test "0 is displayed at the origin" do
       assert 0 == RobotGameView.terrain_number({0, 0})
+    end
+  end
+
+  describe "render terrain.html" do
+    test "you can render terrain" do
+      html = render_to_string(RobotGameView, "terrain.html", terrain: :normal, location: {0, 0})
+
+      [div] = Floki.find(html, "div.terrain.normal")
+      assert ["grid-row: 1; grid-column: 1;"] == Floki.attribute(div, "style")
+      [number] = Floki.find(div, ".number")
+      assert Floki.text(number) == "0"
+    end
+
+    test "Terrain that is not on an edge doesn't have a number" do
+      html = render_to_string(RobotGameView, "terrain.html", terrain: :normal, location: {1, 1})
+      [number] = Floki.find(html, "div.terrain .number")
+      assert Floki.text(number) == ""
+    end
+
+    test "you can render all the types of terrains" do
+      [:normal, :spawn, :obstacle, :inaccesible]
+      |> Enum.map(fn terrain ->
+        html = render_to_string(RobotGameView, "terrain.html", terrain: terrain, location: {1, 1})
+        assert [div] = Floki.find(html, ".terrain.#{terrain}")
+      end)
     end
   end
 end
