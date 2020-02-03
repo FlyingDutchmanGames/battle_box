f0a9bc2376ed3d141b39f83bb88e7f2f9d836846
Grant Powell <gjamespowell@gmail.com>
2020-02-03 00:30:25 -0500

    A VERY basic matchmaker

--- lib/battle_box/games/robot_game/robot_game_supervisor.ex
+++ lib/battle_box/games/robot_game/robot_game_supervisor.ex
@@ -1,6 +1,7 @@
 defmodule BattleBox.Games.RobotGame.RobotGameSupervisor do
   alias BattleBox.Games.RobotGame.GameSupervisor, as: GameSup
   alias BattleBox.Games.RobotGame.PlayerSupervisor, as: PlayerSup
+  alias BattleBox.Games.RobotGame.GameServer
   use Supervisor
 
   @default_name RobotGame
@@ -12,6 +13,8 @@ defmodule BattleBox.Games.RobotGame.RobotGameSupervisor do
 
   def init(%{name: name}) do
     children = [
+      {BattleBox.MatchMaker,
+       %{name: matchmaker_name(name), game_server_supervisor: game_supervisor_name(name)}},
       {GameSup, name: game_supervisor_name(name)},
       {PlayerSup, name: player_supervisor_name(name)}
     ]
@@ -19,6 +22,11 @@ defmodule BattleBox.Games.RobotGame.RobotGameSupervisor do
     Supervisor.init(children, strategy: :one_for_one)
   end
 
-  def game_supervisor_name(name), do: Module.concat(name, GameSupervisor)
-  def player_supervisor_name(name), do: Module.concat(name, PlayerSupervisor)
+  def start_game_server(%{player_1: _, player_2: _, game: _} = opts, name \\ @default_name) do
+    DynamicSupervisor.start_child(game_supervisor_name(name), {GameServer, opts})
+  end
+
+  def game_supervisor_name(name \\ @default_name), do: Module.concat(name, GameSupervisor)
+  def player_supervisor_name(name \\ @default_name), do: Module.concat(name, PlayerSupervisor)
+  def matchmaker_name(name \\ @default_name), do: Module.concat(name, MatchMaker)
 end
---
+++ lib/battle_box/matchmaker/matchmaker.ex
@@ -0,0 +1,39 @@
+defmodule BattleBox.MatchMaker do
+  use GenServer
+  alias BattleBox.Games.RobotGame.{Game, RobotGameSupervisor}
+
+  def join_matchmaker_queue(player_id, pid, matchmaker) do
+    GenServer.call(matchmaker, {:matchmake, player_id, pid})
+  end
+
+  def start_link(%{game_server_supervisor: _} = options) do
+    GenServer.start_link(__MODULE__, options, name: options[:name])
+  end
+
+  def init(data) do
+    data = Map.put(data, :pending_matches, [])
+    {:ok, data}
+  end
+
+  def handle_call({:matchmake, player_id, pid}, _from, %{pending_matches: []} = data) do
+    {:reply, :ok, put_in(data.pending_matches, [{player_id, pid}])}
+  end
+
+  def handle_call(
+        {:matchmake, player_1_id, pid_1},
+        _from,
+        %{pending_matches: [{player_2_id, pid_2}]} = data
+      ) do
+    game =
+      Game.new(
+        id: Ecto.UUID.generate(),
+        player_1: player_1_id,
+        player_2: player_2_id
+      )
+
+    {:ok, _pid} =
+      RobotGameSupervisor.start_game_server(%{player_1: pid_1, player_2: pid_2, game: game})
+
+    {:reply, :ok, put_in(data.pending_matches, [])}
+  end
+end
