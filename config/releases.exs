ae7fe4d403a52280bfcd5f35b0d953d3c3028175
Grant Powell <gjamespowell@gmail.com>
2020-02-24 22:19:27 -0500

    Add host name to config

--- config/releases.exs
+++ config/releases.exs
@@ -2,10 +2,11 @@ import Config
 
 envs =
   [
-    username: "BATTLE_BOX_DB_USER",
-    password: "BATTLE_BOX_DB_PASS",
-    hostname: "BATTLE_BOX_DB_HOST",
-    database: "BATTLE_BOX_DB_DATABASE",
+    host: "HOST",
+    db_username: "BATTLE_BOX_DB_USER",
+    db_password: "BATTLE_BOX_DB_PASS",
+    db_hostname: "BATTLE_BOX_DB_HOST",
+    database_name: "BATTLE_BOX_DB_DATABASE",
     secret_key_base: "BATTLE_BOX_SECRET_KEY_BASE",
     live_view_salt: "BATTLE_BOX_LIVE_VIEW_SALT",
     github_client_id: "BATTLE_BOX_GITHUB_CLIENT_ID",
@@ -14,14 +15,15 @@ envs =
   |> Enum.map(fn {key, env} -> {key, System.fetch_env!(env)} end)
 
 config :battle_box, BattleBox.Repo,
-  username: envs[:username],
-  hostname: envs[:hostname],
-  password: envs[:password],
-  database: envs[:database],
+  username: envs[:db_username],
+  hostname: envs[:db_hostname],
+  password: envs[:db_password],
+  database: envs[:database_name],
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 config :battle_box, BattleBoxWeb.Endpoint,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
+  url: [host: host],
   secret_key_base: envs[:secret_key_base],
   live_view: [signing_salt: envs[:live_view_salt]]
 
