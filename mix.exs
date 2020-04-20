94687e757233187a65c8106f818cd565c8df0245
Grant Powell <gjamespowell@gmail.com>
2020-04-20 00:06:04 -0400

    Add 'gun' http client library

--- mix.exs
+++ mix.exs
@@ -48,7 +48,8 @@ defmodule BattleBox.MixProject do
       {:gen_state_machine, "~> 2.0"},
       {:httpoison, "~> 1.6"},
       {:bypass, "~> 1.0", only: :test},
-      {:observer_cli, "~> 1.5"}
+      {:observer_cli, "~> 1.5"},
+      {:gun, "~> 1.3", only: [:dev, :test]},
     ]
   end
 
