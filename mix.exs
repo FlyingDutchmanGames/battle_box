d475f03fe1bbe17756a75a5dc9026302c3579e48
Grant Powell <gjamespowell@gmail.com>
2020-04-25 16:33:04 -0400

    Explicitly require phoenix live view

--- mix.exs
+++ mix.exs
@@ -31,6 +31,7 @@ defmodule BattleBox.MixProject do
       {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
+      {:phoenix_live_view, "~> 0.12.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_dashboard, "~> 0.2.0"},
       {:telemetry_metrics, "~> 0.4"},
