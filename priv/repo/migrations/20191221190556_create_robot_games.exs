5e0b9ef9ba229e2d91995309ab980dc19e11da9c
Grant Powell <gjamespowell@gmail.com>
2020-05-03 11:00:05 -0400

    Add the settings onto the robot game table

--- priv/repo/migrations/20191221190556_create_robot_games.exs
+++ priv/repo/migrations/20191221190556_create_robot_games.exs
@@ -5,8 +5,15 @@ defmodule BattleBox.Repo.Migrations.CreateRobotGames do
     create table("robot_games") do
       add :events, {:array, :binary}, null: false, default: []
       add :turn, :integer, null: false
-      add :settings_id, :uuid, null: false
       add :game_id, :uuid, null: true
+      add :robot_hp, :integer, null: false
+      add :spawn_every, :integer, null: false
+      add :spawn_per_player, :integer, null: false
+      add :attack_damage, :jsonb, null: false
+      add :collision_damage, :jsonb, null: false
+      add :suicide_damage, :jsonb, null: false
+      add :max_turns, :integer, null: false
+      add :terrain, :binary, null: false
       timestamps()
     end
 
