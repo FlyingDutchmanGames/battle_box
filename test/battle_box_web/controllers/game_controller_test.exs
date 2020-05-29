4dd2954191eccedb2b40d226fad690fbcd2cdab2
Grant Powell <gjamespowell@gmail.com>
2020-05-29 02:19:55 -0400

    Add scores to the pages

--- test/battle_box_web/controllers/game_controller_test.exs
+++ test/battle_box_web/controllers/game_controller_test.exs
@@ -1,11 +1,12 @@
 defmodule BattleBoxWeb.GameControllerTest do
   use BattleBoxWeb.ConnCase, async: false
 
+  test "it will not 404 with a not real lobby", %{conn: conn} do
+    conn
+    |> get("/lobbies/FAKE/games")
+    |> html_response(200)
+  end
+
   describe "index" do
-    test "it will not 404 with a not real lobby", %{conn: conn} do
-      conn
-      |> get("/lobbies/FAKE/games")
-      |> html_response(200)
-    end
   end
 end
