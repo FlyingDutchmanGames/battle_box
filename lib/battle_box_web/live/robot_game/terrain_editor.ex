7d12c0e9411bb9f431f2f057f212e42faea3199a
Grant Powell <gjamespowell@gmail.com>
2020-06-03 22:43:38 -0400

    Switch to using buttons instead of a range slider
    Its really annoying liveview can't handle the nested forms!
    
    (that I can tell, I could be doing something wrong)

--- lib/battle_box_web/live/robot_game/terrain_editor.ex
+++ lib/battle_box_web/live/robot_game/terrain_editor.ex
@@ -19,7 +19,7 @@ defmodule BattleBoxWeb.Live.RobotGame.TerrainEditor do
   end
 
   def handle_event("random", _event, %{assigns: %{rows: rows, cols: cols}} = socket) do
-    data = IO.iodata_to_binary(for i <- 1..(rows * cols), do: <<:random.uniform(3) - 1>>)
+    data = IO.iodata_to_binary(for _ <- 1..(rows * cols), do: <<:random.uniform(3) - 1>>)
     terrain = <<rows::8, cols::8, data::binary>>
     {:noreply, assign_terrain(socket, terrain)}
   end
@@ -34,8 +34,8 @@ defmodule BattleBoxWeb.Live.RobotGame.TerrainEditor do
       for(
         r <- (row - brush)..(row + brush),
         c <- (col - brush)..(col + brush),
-        r > 0,
-        c > 0,
+        r > -1,
+        c > -1,
         r < socket.assigns.rows,
         c < socket.assigns.cols,
         do: [r, c]
@@ -47,16 +47,30 @@ defmodule BattleBoxWeb.Live.RobotGame.TerrainEditor do
     {:noreply, assign_terrain(socket, terrain)}
   end
 
-  def handle_event("set-dimensions", %{"rows" => rows, "cols" => cols}, socket) do
-    rows = String.to_integer(rows)
-    cols = String.to_integer(cols)
-    terrain = Terrain.resize(socket.assigns.terrain, rows, cols)
-    {:noreply, assign_terrain(socket, terrain)}
+  def handle_event("decrement-" <> thing, _events, socket)
+      when thing in ["rows", "cols", "brush"] do
+    thing = String.to_existing_atom(thing)
+    {:noreply, assign(socket, thing, max(socket.assigns[thing] - 1, 1))}
   end
 
-  def handle_event("set-brush-width", %{"brush-width" => brush_width}, socket) do
-    brush_width = String.to_integer(brush_width)
-    {:noreply, assign(socket, brush_width: brush_width)}
+  def handle_event("increment-" <> thing, _events, socket)
+      when thing in ["rows", "cols", "brush"] do
+    thing = String.to_existing_atom(thing)
+
+    case thing do
+      :brush ->
+        {:noreply, assign(socket, :brush_width, min(3, socket.assigns.brush_width + 1))}
+
+      :rows ->
+        rows = min(39, socket.assigns.rows + 1)
+        terrain = Terrain.resize(socket.assigns.terrain, rows, socket.assigns.cols)
+        {:noreply, assign_terrain(socket, terrain)}
+
+      :cols ->
+        cols = min(39, socket.assigns.cols + 1)
+        terrain = Terrain.resize(socket.assigns.terrain, socket.assigns.rows, cols)
+        {:noreply, assign_terrain(socket, terrain)}
+    end
   end
 
   def render(assigns) do
