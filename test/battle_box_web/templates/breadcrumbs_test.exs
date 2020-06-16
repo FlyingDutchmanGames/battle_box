ddcd1519b7a8e595762bcb248a4fc496d39ebc44
Grant Powell <gjamespowell@gmail.com>
2020-06-16 03:08:00 -0400

    Add link tests to bread crumbs

--- test/battle_box_web/templates/breadcrumbs_test.exs
+++ test/battle_box_web/templates/breadcrumbs_test.exs
@@ -10,38 +10,64 @@ defmodule BattleBoxWeb.Templates.BreadCrumbsTest do
   test "it renders the breadcrumbs correctly" do
     [
       # The Basics
-      {[], nil, "/ "},
-      {["foo"], [], "/ foo /"},
-      {[{"foo", "bar"}], [], "/ foo /"},
-      {[], [{"foo", ""}], "/ { foo, }"},
-      {[], [{"foo", ""}, {"bar", ""}], "/ { foo, bar, }"},
+      {[], nil, "/ ", ["/"]},
+      {["foo"], [], "/ foo /", ["/"]},
+      {[{"foo", "/to/foo"}], [], "/ foo /", ["/", "/to/foo"]},
+      {[], [{"foo", "/to/foo"}], "/ { foo, }", ["/", "/to/foo"]},
+      {[], [{"foo", "/to/foo"}, {"bar", "/to/bar"}], "/ { foo, bar, }",
+       ["/", "/to/foo", "/to/bar"]},
+      {[], [{:inaccessible, "foo"}], "/ { foo, }", ["/"]},
       # User
-      {[@user], [], "/ Users / example-username /"},
-      {[{@user, :bots}], [], "/ Users / example-username / Bots /"},
-      {[{@user, :keys}], [], "/ Users / example-username / Keys /"},
-      {[{@user, :arenas}], [], "/ Users / example-username / Arenas /"},
+      {[@user], [], "/ Users / example-username /", ["/", "/users", "/users/example-username"]},
+      {[{@user, :bots}], [], "/ Users / example-username / Bots /",
+       ["/", "/users", "/users/example-username", "/users/example-username/bots"]},
+      {[{@user, :keys}], [], "/ Users / example-username / Keys /",
+       ["/", "/users", "/users/example-username", "/keys"]},
+      {[{@user, :arenas}], [], "/ Users / example-username / Arenas /",
+       ["/", "/users", "/users/example-username", "/users/example-username/arenas"]},
       # Resources
-      {[@bot], [], "/ Users / example-username / Bots / example-bot /"},
-      {[@arena], [], "/ Users / example-username / Arenas / example-arena /"},
+      {[@bot], [], "/ Users / example-username / Bots / example-bot /",
+       [
+         "/",
+         "/users",
+         "/users/example-username",
+         "/users/example-username/bots",
+         "/users/example-username/bots/example-bot"
+       ]},
+      {[@arena], [], "/ Users / example-username / Arenas / example-arena /",
+       [
+         "/",
+         "/users",
+         "/users/example-username",
+         "/users/example-username/arenas",
+         "/users/example-username/arenas/example-arena"
+       ]},
       # Admin
-      {[:admin], [], "/ Admin /"},
-      {[:admin, {:admin, @user}], [], "/ Admin / Users / example-username /"},
+      {[:admin], [], "/ Admin /", ["/", "/admin"]},
+      {[:admin, {:admin, @user}], [], "/ Admin / Users / example-username /",
+       ["/", "/admin", "/admin/users", "/admin/users/example-username"]},
       # Options
-      {[], [{:new, :bot}], "/ { New, }"},
-      {[], [{:new, :arena}], "/ { New, }"},
-      {[], [{:new, :api_key}], "/ { New, }"},
-      {[], [{:edit, @bot}], "/ { Edit, }"},
-      {[], [{:edit, @arena}], "/ { Edit, }"},
-      {[], [{:admin, {:edit, @user}}], "/ { Edit, }"},
-      {[], [{:games, @bot}], "/ { Games, }"},
-      {[], [{:games, @user}], "/ { Games, }"},
-      {[], [{:games, @arena}], "/ { Games, }"},
-      {[], [{:follow, @bot}], "/ { Follow, }"},
-      {[], [{:follow, @user}], "/ { Follow, }"},
-      {[], [{:follow, @arena}], "/ { Follow, }"},
-      {[], [{:inaccessible, "foo"}], "/ { foo, }"}
+      {[], [{:new, :bot}], "/ { New, }", ["/", "/bots/new"]},
+      {[], [{:new, :arena}], "/ { New, }", ["/", "/arenas/new"]},
+      {[], [{:new, :api_key}], "/ { New, }", ["/", "/keys/new"]},
+      {[], [{:edit, @bot}], "/ { Edit, }",
+       ["/", "/users/example-username/bots/example-bot/edit"]},
+      {[], [{:edit, @arena}], "/ { Edit, }",
+       ["/", "/users/example-username/arenas/example-arena/edit"]},
+      {[], [{:admin, {:edit, @user}}], "/ { Edit, }",
+       ["/", "/admin/users/example-username/edit"]},
+      {[], [{:games, @bot}], "/ { Games, }",
+       ["/", "/users/example-username/bots/example-bot/games"]},
+      {[], [{:games, @user}], "/ { Games, }", ["/", "/users/example-username/games"]},
+      {[], [{:games, @arena}], "/ { Games, }",
+       ["/", "/users/example-username/arenas/example-arena/games"]},
+      {[], [{:follow, @bot}], "/ { Follow, }",
+       ["/", "/users/example-username/bots/example-bot/follow"]},
+      {[], [{:follow, @user}], "/ { Follow, }", ["/", "/users/example-username/follow"]},
+      {[], [{:follow, @arena}], "/ { Follow, }",
+       ["/", "/users/example-username/arenas/example-arena/follow"]}
     ]
-    |> Enum.each(fn {nav_segments, nav_options, expected} ->
+    |> Enum.each(fn {nav_segments, nav_options, expected, links} ->
       {:safe, io_list} =
         PageView.render("_bread_crumbs.html",
           conn: BattleBoxWeb.Endpoint,
@@ -57,6 +83,11 @@ defmodule BattleBoxWeb.Templates.BreadCrumbsTest do
                |> Floki.text()
                |> (&Regex.replace(~r/\n/, &1, "")).()
                |> (&Regex.replace(~r/\s+/, &1, " ")).()
+
+      assert links ==
+               document
+               |> Floki.find("a")
+               |> Enum.flat_map(&Floki.attribute(&1, "href"))
     end)
   end
 end
