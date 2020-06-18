6be09049154861864d97f3cdeab557170c51bc99
Grant Powell <gjamespowell@gmail.com>
2020-06-18 02:33:30 -0400

    Allow user and bot level querying in the game controller

--- lib/battle_box_web/controllers/game_controller.ex
+++ lib/battle_box_web/controllers/game_controller.ex
@@ -1,32 +1,7 @@
 defmodule BattleBoxWeb.GameController do
   use BattleBoxWeb, :controller
-  alias BattleBox.{Repo, Game, Arena, GameEngine}
+  alias BattleBox.{Repo, Game, Bot, User, Arena, GameEngine}
   import Ecto.Query
-
-  def index(conn, %{"arena_name" => arena_name} = params) do
-    arena =
-      Repo.get_by(Arena, name: arena_name)
-      |> Repo.preload(:user)
-
-    games =
-      Game
-      |> order_by(desc: :inserted_at)
-      |> filter_arenas(params)
-      |> paginate(params)
-      |> preload(game_bots: [bot: :user])
-      |> Repo.all()
-
-    pagination_info = pagination_info(params)
-    to_page = to_page(conn, params, pagination_info)
-
-    render(conn, "index.html", %{
-      pagination_info: pagination_info,
-      games: games,
-      to_page: to_page,
-      arena: arena,
-      nav_segments: [arena, "Games"]
-    })
-  end
 
   def show(conn, %{"id" => id} = params) do
     game_pid =
@@ -43,23 +18,93 @@ defmodule BattleBoxWeb.GameController do
     )
   end
 
-  defp filter_arenas(query, %{"arena_name" => arena_name}) do
-    query
-    |> join(:inner, [game], arena in assoc(game, :arena), as: :arena)
-    |> where([_game, arena: arena], arena.name == ^arena_name)
-    |> preload([_game, arena: arena], arena: arena)
+  def index(conn, params) do
+    with {:ok, subject} <- load_subject(params) do
+      games =
+        Game
+        |> order_by(desc: :inserted_at)
+        |> paginate(params)
+        |> filter(subject)
+        |> Repo.all()
+        |> Repo.preload(game_bots: [bot: :user], arena: [:user])
+
+      pagination_info = pagination_info(params)
+      to_page = to_page(conn, subject, pagination_info)
+
+      render(conn, "index.html", %{
+        pagination_info: pagination_info,
+        games: games,
+        to_page: to_page,
+        nav_segments: [subject, "Games"]
+      })
+    else
+      {:error, {:not_found, subject}} -> render404(conn, subject)
+    end
   end
 
-  defp filter_arenas(query, _), do: preload(query, :arena)
+  defp filter(query, subject) do
+    case subject do
+      %Arena{id: arena_id} ->
+        query
+        |> where(arena_id: ^arena_id)
 
-  defp to_page(conn, %{"user_username" => username, "arena_name" => arena_name}, %{
-         per_page: per_page
-       }) do
-    fn page ->
-      Routes.user_arena_game_path(conn, :index, username, arena_name, %{
-        page: page,
-        per_page: per_page
-      })
+      %Bot{id: bot_id} ->
+        query
+        |> join(:inner, [game], game_bot in assoc(game, :game_bots), as: :game_bot)
+        |> where([_game, game_bot: game_bot], game_bot.bot_id == ^bot_id)
+
+      %User{id: user_id} ->
+        query
+        |> join(:inner, [game], game_bot in assoc(game, :game_bots), as: :game_bot)
+        |> join(:inner, [_game, game_bot: game_bot], bot in assoc(game_bot, :bot), as: :bot)
+        |> where([_game, bot: bot], bot.user_id == ^user_id)
+    end
+  end
+
+  defp load_subject(params) do
+    case params do
+      %{"arena_name" => arena_name, "user_username" => username} ->
+        Arena
+        |> where(name: ^arena_name)
+        |> join(:inner, [arena], user in assoc(arena, :user), as: :user)
+        |> where([_arena, user: user], user.username == ^username)
+        |> preload(:user)
+        |> Repo.one()
+        |> case do
+          nil -> {:error, {:not_found, {Arena, arena_name, username}}}
+          arena -> {:ok, arena}
+        end
+
+      %{"bot_name" => bot_name, "user_username" => username} ->
+        Bot
+        |> where(name: ^bot_name)
+        |> join(:inner, [bot], user in assoc(bot, :user), as: :user)
+        |> where([_bot, user: user], user.username == ^username)
+        |> preload(:user)
+        |> Repo.one()
+        |> case do
+          nil -> {:error, {:not_found, {Bot, bot_name, username}}}
+          bot -> {:ok, bot}
+        end
+
+      %{"user_username" => username} ->
+        case Repo.get_by(User, username: username) do
+          nil -> {:error, {:not_found, {User, username}}}
+          user -> {:ok, user}
+        end
+    end
+  end
+
+  defp to_page(conn, subject, %{per_page: per_page}) do
+    case subject do
+      %Arena{user: %{username: username}, name: name} ->
+        &Routes.user_arena_game_path(conn, :index, username, name, %{page: &1, per_page: per_page})
+
+      %Bot{user: %{username: username}, name: name} ->
+        &Routes.user_bot_game_path(conn, :index, username, name, %{page: &1, per_page: per_page})
+
+      %User{username: username} ->
+        &Routes.user_game_path(conn, :index, username, %{page: &1, per_page: per_page})
     end
   end
 end
