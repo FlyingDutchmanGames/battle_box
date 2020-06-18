2e4a38572f8dbd1941dcf2804eafb3b49633868f
Grant Powell <gjamespowell@gmail.com>
2020-06-17 23:28:49 -0400

    Update the follow controller

--- lib/battle_box_web/controllers/follow_controller.ex
+++ lib/battle_box_web/controllers/follow_controller.ex
@@ -1,38 +1,51 @@
 defmodule BattleBoxWeb.FollowController do
   use BattleBoxWeb, :controller
-  alias BattleBoxWeb.PageView
   alias BattleBox.{Repo, Arena, User, Bot}
 
   def follow(conn, %{"user_username" => username} = params) do
     case Repo.get_by(User, username: username) do
-      nil -> render404(conn, username: username)
-      user -> render(conn, "follow.html", hydrate_params(params, user))
+      nil ->
+        render404(conn, {User, username})
+
+      user ->
+        case params do
+          %{"arena_name" => name} ->
+            case Repo.get_by(Arena, name: name, user_id: user.id) do
+              nil ->
+                render404(conn, {Arena, name, username})
+
+              arena ->
+                arena = Repo.preload(arena, :user)
+
+                render(conn, "follow.html", %{
+                  nav_segments: [arena, "Follow"],
+                  follow: {Arena, arena.id},
+                  follow_back: %{"user" => username, "arena" => name}
+                })
+            end
+
+          %{"bot_name" => name} ->
+            case Repo.get_by(Bot, name: name, user_id: user.id) do
+              nil ->
+                render404(conn, {Bot, name, username})
+
+              bot ->
+                bot = Repo.preload(bot, :user)
+
+                render(conn, "follow.html", %{
+                  nav_segments: [bot, "Follow"],
+                  follow: {Bot, bot.id},
+                  follow_back: %{"user" => username, "bot" => name}
+                })
+            end
+
+          _ ->
+            render(conn, "follow.html", %{
+              nav_segments: [user, "Follow"],
+              follow: {User, user.id},
+              follow_back: %{"user" => username}
+            })
+        end
     end
-  end
-
-  defp render404(conn, username: username) do
-    conn
-    |> put_status(404)
-    |> put_view(PageView)
-    |> render("not_found.html", message: "User (#{username}) not found")
-  end
-
-  defp hydrate_params(params, user) do
-    hydrated =
-      params
-      |> Enum.map(fn
-        {"arena_name", name} -> {:arena, Repo.get_by(Arena, name: name, user_id: user.id)}
-        {"bot_name", name} -> {:bot, Repo.get_by(Bot, name: name, user_id: user.id)}
-        _ -> nil
-      end)
-      |> Enum.reject(&is_nil/1)
-      |> Enum.map(fn {key, resource} -> {key, Repo.preload(resource, :user)} end)
-      |> Map.new()
-
-    lead_segment = hydrated[:arena] || hydrated[:bot] || user
-
-    %{user: user, arena: nil, bot: nil}
-    |> Map.merge(hydrated)
-    |> Map.put(:nav_segments, [lead_segment, "Follow"])
   end
 end
--- lib/battle_box_web/live/follow.ex
+++ lib/battle_box_web/live/follow.ex
@@ -1,45 +1,39 @@
 defmodule BattleBoxWeb.Live.Follow do
   use BattleBoxWeb, :live_view
-  alias BattleBox.GameEngine
-  alias BattleBoxWeb.FollowView
+  alias BattleBox.{GameEngine, Bot, Arena, User}
 
-  def mount(_params, session, socket) do
+  def mount(_params, %{"follow" => {type, id}} = session, socket)
+      when is_binary(id) and type in [Bot, User, Arena] do
     if connected?(socket) do
-      case session do
-        %{"arena" => %{id: id}} ->
-          GameEngine.subscribe_to_arena_events(game_engine(), id, [:game_started, :game_update])
+      func =
+        case type do
+          Bot -> :subscribe_to_bot_events
+          User -> :subscribe_to_user_events
+          Arena -> :subscribe_to_arena_events
+        end
 
-        %{"bot" => %{id: id}} ->
-          GameEngine.subscribe_to_bot_events(game_engine(), id, [:game_started, :game_update])
-
-        %{"user" => %{id: id}} ->
-          GameEngine.subscribe_to_user_events(game_engine(), id, [:game_started, :game_update])
-      end
+      :ok = apply(GameEngine, func, [game_engine(), id, [:game_started, :game_update]])
     end
 
-    {:ok, assign(socket, follow_params: session)}
+    {:ok,
+     assign(socket,
+       follow_back: session["follow_back"],
+       message: session["message"] || "Waiting for Game to Start"
+     )}
   end
 
   def handle_info({_topic, event, game_id}, socket) when event in [:game_update, :game_started] do
-    keep_following =
-      case socket.assigns.follow_params do
-        %{"arena" => %{name: name}, "user" => %{username: username}} ->
-          %{arena: name, user: username}
-
-        %{"bot" => %{name: name}, "user" => %{username: username}} ->
-          %{bot: name, user: username}
-
-        %{"user" => %{username: username}} ->
-          %{user: username}
-      end
-
     {:noreply,
      redirect(socket,
-       to: Routes.game_path(socket, :show, game_id, %{follow: keep_following})
+       to: Routes.game_path(socket, :show, game_id, %{follow: socket.assigns.follow_back})
      )}
   end
 
   def render(assigns) do
-    FollowView.render("_follow.html", assigns)
+    ~L"""
+    <div style="display: flex; justify-content: center; padding: 10px;">
+      <h1 class="blinking"><%= @message %></h1>
+    </div>
+    """
   end
 end
