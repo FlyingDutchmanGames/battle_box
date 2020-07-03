da90de5eddea28a487209bf9f018d27700fe8862
Grant Powell <gjamespowell@gmail.com>
2020-07-03 00:21:35 -0400

    Add in the honey badger strategy

--- lib/battle_box/games/robot_game/ais/strategy.ex
+++ lib/battle_box/games/robot_game/ais/strategy.ex
@@ -1,4 +1,37 @@
 defmodule BattleBox.Games.RobotGame.Ais.Strategy do
+  defmodule Utilites do
+    def manhattan_distance(%{"location" => location}, other),
+      do: manhattan_distance(location, other)
+
+    def manhattan_distance(other, %{"location" => location}),
+      do: manhattan_distance(other, location)
+
+    def manhattan_distance([x1, y1], [x2, y2]) do
+      a_squared = :math.pow(x2 - x1, 2)
+      b_squared = :math.pow(y2 - y1, 2)
+      :math.pow(a_squared + b_squared, 0.5)
+    end
+
+    def towards(%{location: location}, other), do: towards(location, other)
+    def towards(other, %{location: location}), do: towards(other, location)
+
+    def towards([x1, y1] = _robot_loc, [x2, y2] = _target) do
+      cond do
+        x1 > x2 ->
+          [x1 - 1, y1]
+
+        x1 < x2 ->
+          [x1 + 1, y1]
+
+        y1 > y2 ->
+          [x1, y1 - 1]
+
+        y1 < y2 ->
+          [x1, y1 + 1]
+      end
+    end
+  end
+
   defmodule Moves do
     def guard(%{id: robot_id}), do: %{"type" => "guard", "robot_id" => robot_id}
     def suicide(%{id: robot_id}), do: %{"type" => "suicide", "robot_id" => robot_id}
@@ -15,7 +48,8 @@ defmodule BattleBox.Games.RobotGame.Ais.Strategy do
 
   defmacro __using__(_opts) do
     quote do
-      import BattleBox.Games.RobotGame.Ais.Strategy.Moves
+      import BattleBox.Games.RobotGame.Ais.Strategy.{Moves, Utilites}
+      import BattleBox.Games.RobotGame, only: [adjacent_locations: 1]
       alias BattleBox.Games.RobotGame.Settings.Terrain
     end
   end
