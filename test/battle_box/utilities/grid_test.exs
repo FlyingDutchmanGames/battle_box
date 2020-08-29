a11a22722f37e50ae940c1c5a34c0002e02f2d72
Grant Powell <gjamespowell@gmail.com>
2020-08-28 22:58:16 -0400

    Add more grid tests

--- test/battle_box/utilities/grid_test.exs
+++ test/battle_box/utilities/grid_test.exs
@@ -4,11 +4,16 @@ defmodule BattleBox.Utilities.GridTest do
   alias BattleBox.Utilities.Grid
 
   describe "manhattan_distance/2" do
-    test "it gives the manhattan_distance between two points" do
-      assert Grid.manhattan_distance([0, 0], [0, 0]) == 0.0
-      assert Grid.manhattan_distance([0, 0], [0, 1]) == 1.0
-      assert Grid.manhattan_distance([0, 0], [0, -1]) == 1.0
-      assert Grid.manhattan_distance([0, 0], [3, 4]) == 5.0
+    for {point_a, point_b, distance} <- [
+          {[0, 0], [0, 0], 0.0},
+          {[0, 0], [0, 1], 1.0},
+          {[1, 0], [0, 0], 1.0},
+          {[0, 0], [0, -1], 1.0},
+          {[0, 0], [3, 4], 5.0}
+        ] do
+      test "manhattan_distance(#{inspect(point_a)}, #{inspect(point_b)}) => #{inspect(distance)}" do
+        assert Grid.manhattan_distance(unquote(point_a), unquote(point_b)) == unquote(distance)
+      end
     end
   end
 end
