3db066ca196d9cc4596ae3cc72c098f9392b97fd
Grant Powell <gjamespowell@gmail.com>
2020-08-29 10:50:02 -0400

    Add a games toc to the docs

--- lib/battle_box_web/views/docs_view.ex
+++ lib/battle_box_web/views/docs_view.ex
@@ -1,3 +1,5 @@
 defmodule BattleBoxWeb.DocsView do
   use BattleBoxWeb, :view
+  import BattleBox.InstalledGames, only: [installed_games: 0]
+  import BattleBox.Utilities.Humanize, only: [kebabify: 1]
 end
