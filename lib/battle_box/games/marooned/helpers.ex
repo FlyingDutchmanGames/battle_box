ceb24a13c37c43be0b7cab232720b44adc682e9d
Grant Powell <gjamespowell@gmail.com>
2020-08-30 23:34:36 -0400

    A skelton for a marooned helper

--- lib/battle_box/games/marooned/helpers.ex
+++ lib/battle_box/games/marooned/helpers.ex
@@ -1,5 +1,28 @@
 defmodule BattleBox.Games.Marooned.Helpers do
-  def sigil_m(map, _modifiers) do
-    raise "foo"
+  alias BattleBox.Games.Marooned
+
+  def sigil_m(game, _modifiers) do
+    graphs =
+      game
+      |> String.split("\n")
+      |> Enum.map(&String.graphemes/1)
+      |> Enum.reject(fn x -> x == [] end)
+      |> Enum.map(&Enum.reject(&1, fn grapheme -> String.trim(grapheme) == "" end))
+
+    if length(Enum.uniq(for row <- graphs, do: length(row))) != 1,
+      do: raise("Invalid Dimensions")
+
+    rows = length(graphs)
+    cols = graphs |> Enum.map(&length/1) |> Enum.max(fn -> 0 end)
+
+    graph_with_indexes =
+      for {row, row_num} <- Enum.with_index(Enum.reverse(graphs)),
+          {col, col_num} <- Enum.with_index(row),
+          do: {[row_num, col_num], col}
+
+    %Marooned{
+      rows: rows,
+      cols: cols
+    }
   end
 end
--- test/battle_box/games/marooned/helpers_test.exs
+++ test/battle_box/games/marooned/helpers_test.exs
@@ -1,13 +1,48 @@
 defmodule BattleBox.Games.Marooned.HelpersTest do
   use ExUnit.Case, async: true
+  alias BattleBox.Games.Marooned
 
   import BattleBox.Games.Marooned.Helpers
 
-  test "its an error not to have both players on the board" do
-    assert_raise(RuntimeError, fn ->
-      ~m/0 0 0
+  test "it works" do
+    assert game_data =
+      ~m/0 1 0
+         x 0 x
+         0 x 0
+         0 2 0/
+
+    assert game_data.rows == 4
+    assert game_data.cols == 3
+  end
+
+  describe "errors" do
+    test "its an error not to have both players on the board" do
+      assert_raise(RuntimeError, fn ->
+        ~m/0 0 0
          0 0 0
          0 0 0/
-    end)
+      end)
+    end
+
+    test "its an error to have non uniform length or height" do
+      %{message: message} =
+        assert_raise(RuntimeError, fn ->
+          ~m/0 1 0 0
+           0 0 0
+           0 2 0/
+        end)
+
+      assert message =~ "Invalid Dimensions"
+
+      %{message: message} =
+        assert_raise(RuntimeError, fn ->
+          ~m/0 1 0
+           0 0 0
+           0 2 0
+           0 /
+        end)
+
+      assert message =~ "Invalid Dimensions"
+    end
   end
 end
