a209fe372910885e51736e19abc9e8fe34d24ff2
Grant Powell <gjamespowell@gmail.com>
2020-09-14 21:45:52 -0400

    Some basics of the human server

--- lib/battle_box/game_engine/human_server.ex
+++ lib/battle_box/game_engine/human_server.ex
@@ -1,8 +1,28 @@
 defmodule BattleBox.GameEngine.HumanServer do
+  alias BattleBox.GameEngine.GameServer
   use GenStateMachine, callback_mode: [:handle_event_function], restart: :temporary
+  import :timer, only: [minutes: 1]
+
+  @default_connection_timeout minutes(5)
+
+  def connect_ui(human_server, pid) do
+    GenStateMachine.call(human_server, {:connect_ui, pid})
+  end
+
+  def submit_commands(human_server, commands) do
+    GenStateMachine.call(human_server, {:submit_commands, commands})
+  end
 
   def start_link(%{names: _} = config, opts) do
-    opts = Map.put_new(opts, :human_server_id, Ecto.UUID.generate())
+    opts =
+      Map.merge(
+        %{
+          connection_timeout: @default_connection_timeout,
+          human_server_id: Ecto.UUID.generate(),
+          ui_pid: nil
+        },
+        opts
+      )
 
     GenStateMachine.start_link(__MODULE__, Map.merge(config, opts),
       name:
@@ -13,6 +33,82 @@ defmodule BattleBox.GameEngine.HumanServer do
   end
 
   def init(data) do
-    {:ok, :unconnected, data}
+    {:ok, :awaiting_game_request, data}
+  end
+
+  def handle_event(
+        {:call, from},
+        {:connect_ui, ui_pid},
+        :awaiting_game_request,
+        %{ui_pid: nil} = data
+      ) do
+    data = Map.put(data, :pending_connect_ui, {from, ui_pid})
+    {:keep_state, data}
+  end
+
+  def handle_event({:call, from}, {:connect_ui, _ui_pid}, _state, %{ui_pid: ui_pid})
+      when not is_nil(ui_pid) do
+    {:keep_state_and_data, {:reply, from, {:error, :already_connected}}}
+  end
+
+  def handle_event({:call, from}, {:connect_ui, ui_pid}, state, %{ui_pid: nil} = data) do
+    Process.monitor(ui_pid)
+    data = Map.put(data, :ui_pid, ui_pid)
+    :ok = update_registry(data)
+    {:keep_state, data, {:reply, from, {:ok, on_connect_msg(data)}}}
+  end
+
+  def handle_event(:info, {:game_cancelled, game_id}, _state, data) do
+    if data.ui_pid, do: send(data.ui_pid, {:game_cancelled, game_id})
+    {:stop, :normal}
+  end
+
+  def handle_event(
+        :info,
+        {:game_request, game_request},
+        :awaiting_game_request,
+        %{pending_connect_ui: {from, ui_pid}} = data
+      ) do
+    Process.monitor(game_request.game_server)
+    :ok = GameServer.accept_game(game_request.game_server, game_request.player)
+    data = Map.put(data, :game_request, game_request)
+    :ok = update_registry(data)
+    {:next_state, :playing, data, {:reply, from, {:ok, on_connect_msg(data)}}}
+  end
+
+  def handle_event(:info, {:game_request, game_request}, :awaiting_game_request, data) do
+    Process.monitor(game_request.game_server)
+    :ok = GameServer.accept_game(game_request.game_server, game_request.player)
+    data = Map.put(data, :game_request, game_request)
+    :ok = update_registry(data)
+    {:next_state, :playing, data}
+  end
+
+  def handle_event(
+        :info,
+        {:DOWN, _, _, game_server, _},
+        state,
+        %{game_request: %{game_server: game_server}} = data
+      ) do
+    {:stop, :normal}
+  end
+
+  defp on_connect_msg(data) do
+    %{
+      game_request: data[:game_request],
+      commands_request: data[:commands_request]
+    }
+  end
+
+  defp update_registry(%{names: names} = data) do
+    metadata =
+      data
+      |> Map.take([:ui_pid])
+      |> Map.put(:game_id, data[:game_request][:game_id])
+
+    {_, _} =
+      Registry.update_value(names.human_registry, data.human_server_id, &Map.merge(&1, metadata))
+
+    :ok
   end
 end
--- test/battle_box/game_engine/human_server_test.exs
+++ test/battle_box/game_engine/human_server_test.exs
@@ -1,16 +1,157 @@
 defmodule BattleBox.GameEngine.HumanServerTest do
   use ExUnit.Case, async: true
   use BattleBox.DataCase, async: false
-  alias BattleBox.GameEngine
+  alias BattleBox.{Game, GameEngine, Games.Marooned}
+  alias BattleBox.GameEngine.{GameServer, HumanServer}
+  import BattleBox.TestConvenienceHelpers, only: [named_proxy: 1, named_proxy: 2]
 
   setup %{test: name} do
     {:ok, _} = GameEngine.start_link(name: name)
     {:ok, GameEngine.names(name)}
   end
 
+  setup do
+    {:ok, arena} = marooned_arena()
+
+    game = %Game{
+      id: Ecto.UUID.generate(),
+      arena: arena,
+      arena_id: arena.id,
+      game_type: arena.game_type,
+      game_bots: [],
+      marooned: %Marooned{}
+    }
+
+    %{game: game}
+  end
+
   test "you can start the thing", context do
     opts = %{}
-    {:ok, human_server, _meta} = GameEngine.start_human(context.game_engine, opts)
+
+    {:ok, human_server, %{human_server_id: id}} =
+      GameEngine.start_human(context.game_engine, opts)
+
     assert Process.alive?(human_server)
+
+    assert %{human_server_id: ^id, pid: ^human_server} =
+             GameEngine.get_human_server(context.game_engine, id)
   end
+
+  test "it can accept a game", context do
+    {:ok, human_server, %{human_server_id: human_server_id}} =
+      GameEngine.start_human(context.game_engine, %{})
+
+    {:ok, _game_server} =
+      GameEngine.start_game(context.game_engine, %{
+        game: context.game,
+        players: %{1 => named_proxy(:other_player), 2 => human_server}
+      })
+
+    assert_receive {:other_player,
+                    {:game_request, %{player: player, game_server: game_server, game_id: game_id}}}
+
+    :ok = GameServer.accept_game(game_server, player)
+    Process.sleep(10)
+
+    assert %{game_id: ^game_id} =
+             GameEngine.get_human_server(context.game_engine, human_server_id)
+  end
+
+  test "if the game gets cancelled the human server dies", context do
+    {:ok, human_server, _meta} =
+      GameEngine.start_human(context.game_engine, %{ui_pid: named_proxy(:ui_pid)})
+
+    {:ok, _game_server} =
+      GameEngine.start_game(context.game_engine, %{
+        game: context.game,
+        players: %{1 => named_proxy(:other_player), 2 => human_server}
+      })
+
+    Process.monitor(human_server)
+
+    assert_receive {:other_player,
+                    {:game_request, %{player: player, game_server: game_server, game_id: game_id}}}
+
+    :ok = GameServer.reject_game(game_server, player)
+    Process.sleep(10)
+    assert_receive {:DOWN, _ref, :process, ^human_server, :normal}
+    assert_receive {:ui_pid, {:game_cancelled, ^game_id}}
+  end
+
+  test "if the game server crashes, the human server dies", context do
+    {:ok, human_server, _meta} = GameEngine.start_human(context.game_engine, %{})
+
+    {:ok, game_server} =
+      GameEngine.start_game(context.game_engine, %{
+        game: context.game,
+        players: %{1 => named_proxy(:other_player), 2 => human_server}
+      })
+
+    Process.monitor(human_server)
+    Process.sleep(10)
+    Process.exit(game_server, :kill)
+    assert_receive {:DOWN, _ref, :process, ^human_server, :normal}
+  end
+
+  describe "connect_ui/2" do
+    test "you cannot connect when there is already a connection", context do
+      {:ok, human_server, _mta} =
+        GameEngine.start_human(context.game_engine, %{ui_pid: named_proxy(:ui_pid)})
+
+      assert {:error, :already_connected} =
+               HumanServer.connect_ui(human_server, named_proxy(:nope))
+    end
+
+    test "you can connect a ui pid when there is not one connected", context do
+      {:ok, human_server, _meta} = GameEngine.start_human(context.game_engine, %{})
+
+      {:ok, _game_server} =
+        GameEngine.start_game(context.game_engine, %{
+          game: context.game,
+          players: %{1 => named_proxy(:other_player), 2 => human_server}
+        })
+
+      Process.sleep(10)
+
+      assert {:ok, %{game_request: _, commands_request: _}} =
+               HumanServer.connect_ui(human_server, named_proxy(:ui_pid))
+    end
+
+    test "You cannot connect a ui server until after the game request has arrived", context do
+      test_pid = self()
+      {:ok, human_server, _meta} = GameEngine.start_human(context.game_engine, %{})
+
+      named_proxy(:ui_pid, fn ->
+        assert {:ok, %{game_request: gr, commands_request: _}} =
+                 HumanServer.connect_ui(human_server, named_proxy(:ui_pid))
+
+        assert gr != nil
+        send(test_pid, :success)
+      end)
+
+      refute_receive :success, 100
+
+      {:ok, _game_server} =
+        GameEngine.start_game(context.game_engine, %{
+          game: context.game,
+          players: %{1 => named_proxy(:other_player), 2 => human_server}
+        })
+
+      assert_receive :success
+    end
+
+    # test "if a connection dies, you can reconnect and get the current game state"
+  end
+
+  # describe "submit_commands/2" do
+  #   test "The human server will ask the UI server for commands"
+  #   test "if the ui process crashes, it will ask for the same commands of the new connection"
+  # end
+
+  # describe "inactivity timeouts" do
+  #   test "if no one connects before the connection_timeout, the human server dies"
+  #   test "if someone connects and then disconnects, the server dies after connection_timeout"
+  # end
+
+  # test "when the game is over, it sends a game over message and dies" 
 end
