7987cda8ea661f30d2daed0dd06a84b5e1fe81bf
Grant Powell <gjamespowell@gmail.com>
2020-09-15 00:44:41 -0400

    Remove the game viewer

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,6 @@ import css from "../css/app.css"
 import header_css from "../css/header.css"
 import not_found_css from "../css/not_found.css"
 import settings_css from "../css/settings.css"
-import game_viewer_css from "../css/game_viewer.css"
 import docs_css from "../css/docs.css"
 
 import "phoenix_html"
