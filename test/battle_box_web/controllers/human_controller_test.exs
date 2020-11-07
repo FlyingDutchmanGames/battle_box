ce35030866dd9165f449b7e1a9059627e53f0e7d
Grant Powell <gjamespowell@gmail.com>
2020-11-06 21:45:07 -0500

    Fix the specs for the human controller

--- test/battle_box_web/controllers/human_controller_test.exs
+++ test/battle_box_web/controllers/human_controller_test.exs
@@ -1,6 +1,14 @@
 defmodule BattleBoxWeb.HumanControllerTest do
   use BattleBoxWeb.ConnCase, async: false
   alias BattleBox.Games.Marooned
+  alias BattleBox.InstalledGames
+
+  setup %{conn: conn} do
+    {:ok, user} = create_user()
+    {:ok, arena} = marooned_arena(user: user)
+    conn = signin(conn, user: user)
+    %{arena: arena, conn: conn}
+  end
 
   describe "GET /play" do
     test "it will let you select a game", %{conn: conn} do
@@ -9,24 +17,38 @@ defmodule BattleBoxWeb.HumanControllerTest do
         |> get("/play")
         |> html_response(200)
 
-      assert html =~ "Marooned"
+      for game <- InstalledGames.installed_games() do
+        assert html =~ game.title()
+      end
     end
 
-    test "if you give it a game type, it will let you select an opponent", %{conn: conn} do
+    test "if you give it a game type, it will let you select an arena", %{
+      conn: conn,
+      arena: arena
+    } do
       html =
         conn
-        |> get("/play/marooned")
+        |> get("/play/game_type/marooned")
         |> html_response(200)
 
       assert html =~ "Marooned"
 
-      for ai <- Marooned.ais() do
-        assert html =~ ai
+      for %{name: name} <- Marooned.default_arenas() do
+        assert html =~ name
+      end
+
+      assert html =~ arena.name
+    end
+
+    test "if you select an arena, it will show opponents", %{conn: conn, arena: arena} do
+      html =
+        conn
+        |> get("/play/game_type/marooned/arena/#{arena.name}")
+        |> html_response(200)
+
+      for opponent <- Marooned.ais() do
+        assert html =~ opponent.name()
       end
     end
   end
-
-  # describe "POST /play" do
-  #   test "it will let you start a game, and redirect to it"
-  # end
 end
