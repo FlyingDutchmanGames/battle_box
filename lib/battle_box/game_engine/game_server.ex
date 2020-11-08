55162eb28ed6b2b35223950b622660dcfc7d1f4f
Grant Powell <gjamespowell@gmail.com>
2020-11-08 17:39:09 -0500

    Add score to the commands request

--- lib/battle_box/game_engine/game_server.ex
+++ lib/battle_box/game_engine/game_server.ex
@@ -93,6 +93,7 @@ defmodule BattleBox.GameEngine.GameServer do
         minimum_time: data.game.arena.command_time_minimum_ms,
         maximum_time: data.game.arena.command_time_maximum_ms,
         player: player,
+        score: Game.score(data.game),
         request_id: Ecto.UUID.generate()
       })
     end
