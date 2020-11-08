3dc7fc8edcf2a2b766822e300098698ef7799cd8
Grant Powell <gjamespowell@gmail.com>
2020-11-07 20:06:28 -0500

    Update my web macros

--- lib/battle_box_web.ex
+++ lib/battle_box_web.ex
@@ -2,25 +2,27 @@ defmodule BattleBoxWeb do
   def live_view do
     quote do
       use Phoenix.LiveView
-      alias BattleBoxWeb.Router.Helpers, as: Routes
+      unquote(view_helpers())
+    end
+  end
 
-      @endpoint BattleBoxWeb.Endpoint
-      import BattleBox.GameEngine.Provider, only: [game_engine: 0]
+  def live_component do
+    quote do
+      use Phoenix.LiveComponent
+      unquote(view_helpers())
     end
   end
 
   def controller do
     quote do
-      import Phoenix.LiveView.Controller
       import BattleBoxWeb.Controllers.Helpers
       use Phoenix.Controller, namespace: BattleBoxWeb
 
       import Plug.Conn
       import BattleBoxWeb.Gettext
       alias BattleBoxWeb.Router.Helpers, as: Routes
-      import Phoenix.LiveView.Controller
-      import BattleBox.Utilities.Paginator, only: [paginate: 2, pagination_info: 1]
 
+      import BattleBox.Utilities.Paginator, only: [paginate: 2, pagination_info: 1]
       import BattleBox.GameEngine.Provider, only: [game_engine: 0]
     end
   end
@@ -31,7 +33,8 @@ defmodule BattleBoxWeb do
         root: "lib/battle_box_web/templates",
         namespace: BattleBoxWeb
 
-      import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]
+      import Phoenix.Controller,
+        only: [get_flash: 1, get_flash: 2, view_module: 1, view_template: 1]
 
       use Phoenix.HTML
 
@@ -47,6 +50,7 @@ defmodule BattleBoxWeb do
   def router do
     quote do
       use Phoenix.Router
+
       import Plug.Conn
       import Phoenix.Controller
       import Phoenix.LiveView.Router
@@ -57,6 +61,25 @@ defmodule BattleBoxWeb do
     quote do
       use Phoenix.Channel
       import BattleBoxWeb.Gettext
+    end
+  end
+
+  defp view_helpers do
+    quote do
+      @endpoint BattleBoxWeb.Endpoint
+      # Use all HTML functionality (forms, tags, etc)
+      use Phoenix.HTML
+
+      # Import LiveView helpers (live_render, live_component, live_patch, etc)
+      import Phoenix.LiveView.Helpers
+
+      # Import basic rendering functionality (render, render_layout, etc)
+      import Phoenix.View
+
+      import BattleBoxWeb.ErrorHelpers
+      import BattleBoxWeb.Gettext
+      alias BattleBoxWeb.Router.Helpers, as: Routes
+      import BattleBox.GameEngine.Provider, only: [game_engine: 0]
     end
   end
 
